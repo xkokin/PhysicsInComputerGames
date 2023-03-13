92be1037e9151fd227ffe10af595433b31a9b681
= <=>
2023-03-13 13:37:13 +0100

    uloha4/semi_ready

--- uloha3.c
+++ uloha3.c
@@ -1,53 +1,155 @@
 //Hlib Kokin id:117991
 
+
+#include <GL/glu.h>
+#include<GL/freeglut.h>
+#include<GL/gl.h>
 #include<stdlib.h>
 #include<stdio.h>
 #include <math.h>
 #include <unistd.h>
 
+const int icaskrok = 10; // 10 milisekund
+const float g = 9.80665; //m/s^2
+float a, ysur, vy, ymax, tmax, td, sy; //ymax je najvyssia pointa objektu, sy je startova suradnica pre maticu
+int theta; // uhol ktory zavisi od toho kam bol vyhodeny objekt
+int atHighest; // ako keby boolean premenna ktora povie ci objekt uz bol v najvyssom bode
+
+FILE* file_txt; // zapisujem data do dvoch suborov, txt na odovzdanie a dat na vytvorenie grafu kedze
+// ja nemozem spravit gnuplot z txt
+FILE* file_dat;
+
+const float Lmax = 4.0;
+
+void aktualizuj(const int ihod){
+
+    float t = 0.01*(ihod); //cas od zaciatku;
+
+    if (t >= tmax && atHighest != 1) atHighest = 1;
+
+    if(ysur < 0) {
+        fclose(file_txt);
+        fclose(file_dat);
+        return;
+    }
+
+
+    printf("time in seconds: %.2fs, was at highest %d\n", t, atHighest);
+
+
+
+    if (atHighest == 0 && ysur < ymax) {
+        //ysur -= (vy * sin(theta) * t) - (0.5 * g * t*t); //t je cas od spustenia
+        //vy = -sqrt(2 * g * ysur + vy*vy); // podla rovnice musime tam mat sin^2(theta) ale ked
+        ysur += vy * 0.01;
+        vy = vy - g*0.01;
+        // mozeme mat theta bud 90 alebo 180 tak mozeme vynechat ten stvorec pretoze hodnoty sa nemenia
+    }
+    else {
+        //ysur = ymax - (vmax * t-tmax) + (0.5 * g * pow(t-tmax, 2));
+        //vy = sqrt(2 * g * (ymax - ysur) + pow(vmax, 2));
+        ysur += vy * 0.01; // kedze vy bude zaporna
+        vy = vy - g*0.01;
+    }
+
+
+    fprintf(file_txt, "%.2f\t%.2f\t%.2f\n", t, ysur, vy);
+    fprintf(file_dat, "%.2f\t%.2f\t%.2f\n", t, ysur, vy);
+    glutPostRedisplay();
+
+    glutTimerFunc(icaskrok, aktualizuj, ihod+1);
+}
+
+void Zvisly_Vrh(){
+    glClear(GL_COLOR_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glScalef(a, a, 1);
+    glTranslatef(0, ysur/ymax, 0);
+
+
+    glBegin(GL_QUADS);
+
+    glColor3f(1.0,0.0,0.0);
+    glVertex2f(-0.1, sy+0.2);
+
+    glColor3f(0.5,0.5,0.0);
+    glVertex2f(-0.1 , sy);
+
+    glColor3f(0.0,0.5,0.5);
+    glVertex2f(0.1 , sy);
+
+    glColor3f(0.0,0.0,1.0);
+    glVertex2f(0.1 , sy+0.2);
+
+
+    glEnd();
+
+    glutSwapBuffers();
+
+}
+
+void obsluhaResize(int sirka, int vyska){
+    glViewport(0, 0, sirka, vyska);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    if(sirka == 0) sirka++;
+    const float pomstr = (GLfloat)vyska/(GLfloat)sirka;
+    if (sirka >= vyska) gluOrtho2D(-1.0*pomstr*a, 1.0*pomstr*a, -1.0*a, 1.0*a);
+        //else gluOrtho2D(-0.375*a, 0.375*a, -0.375*pomstr*a, 0.375*pomstr*a);
+    else gluOrtho2D(-0.5, 0.5, -0.5*pomstr*(-1), 0.5*pomstr*ymax);
+    //else gluOrtho2D(-0.5, 0.5, -0.5*pomstr, 0.5*pomstr);
+
+}
 
 
 
 int main(int argc, char **argv){
 
-    const float icaskrok = 0.5;
-    float v1, a1, v2, a2, dst;
-    scanf("%f %f %f %f %f", &v1, &a1, &v2, &a2, &dst);
-    FILE* file;
-    int closed = 0;
+    scanf("%f %f %f", &ysur, &vy, &a);
 
-    file = fopen("uloha2.dat","w");
-    float b = (v1+v2);
-    float a = (a1/2 + a2/2);
-    float c = -dst;
+    if (vy < 0) theta = 0; // ked objekt je vyhodeni hore tak rychlost bude zaporna
+    if (vy >= 0) theta = 1;
 
-    float d = b*b - 4 * a * c;
-    if (d < 0) printf("They will never meet\n");
-    float root1 = ( -b + sqrt(d)) / (2* a);
-    float root2 = ( -b - sqrt(d)) / (2* a);
-    float res = 0;
-    if(root1 < 0) res = root2;
-    else if(root2 < 0) res = root1;
+    file_txt = fopen("uloha3.txt","w");
+    file_dat = fopen("uloha3.dat","w");
+    if (vy > 0) tmax = vy / g;   //cas za ktory stvorec dosihne najvyssej pozicie
+    else tmax = 0.0;
 
-    printf("They will meet in %.2f secs\n"
-           "At x = %.2f meters", res, res*v1);
+    td = (1/g)*(vy+sqrt(vy*vy+2*g*ysur));
+    if (td < 0) td = (1/g)*(vy-sqrt(vy*vy+2*g*ysur));
 
-    fprintf(file, "# Hlavicka #\n"
-                  "# v1=%.2fm/s, a1=%.2fm/s, v2=%.2fm/s, a2=%.2fm/s,\n"
-                  "# dst=%.2fm t=%.2fm, rx = %.2f\n#----------#\n"
-                  ,v1, a2, v2, a2, dst, res, res * v1);
+    ymax = (vy*vy * theta) / (2*g) + ysur; // maximalne dosiahnutelna vyska
+    //vmax = sqrt(vy*vy - 2 * g * ymax * sin(theta)); // rychlost v najvyssem bode
 
-    int x1 = 0;
-    int x2 = 100;
-    float t = 0;
-    while(x2>x1){
-        fprintf(file, "%.1f\t%d\t%d\n", t, x1, x2);
-        sleep(icaskrok);
-        t+=0.5;
-        x1 = v1*t + a1/2*t*t;
-        x2 = 100 - v2*t + a2/2*t*t;
-    }
+    sy = -1 ; // startove koordinaty
 
-    fclose(file);
+    if(ysur == ymax) atHighest = 1;
+
+    printf ("Najvyssia pozicia bude: %.2f\n"
+            "V case %.2f\n"
+            "sy = %.2f\n"
+            "td = %.2f\n", ymax, tmax, sy+ ysur/ymax*2, td);
+
+    fprintf(file_txt, "#-Hlavicka-#\n"
+                      "# y0 = %.2fm, v0y = %.2fm/s, ymax = %.2fm, tmax = %.2fs\n#----------#\n", ysur, vy, ymax, tmax);
+    fprintf(file_txt, "%.2f\t%.2f\t%.2f\n", 0.00, ysur, vy);
+    fprintf(file_dat, "%.2f\t%.2f\t%.2f\n", 0.00, ysur, vy);
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE);
+    glutInitWindowSize(300, 600);
+    glutInitWindowPosition(200,150);
+    glutCreateWindow ("FYPH/Kokin: Zvisly Vrh");
+
+    glutDisplayFunc(Zvisly_Vrh);
+    glClearColor(0.0, 0.0, 1.0, 0.0);
+    glutReshapeFunc(obsluhaResize);
+    glutTimerFunc(icaskrok, aktualizuj, 1);
+    glutMainLoop();
     return 0;
+
 }
--- uloha4/uloha4.c
+++ uloha4/uloha4.c
@@ -10,21 +10,22 @@
 #include <unistd.h>
 
 const int icaskrok = 10; // 10 milisekund cas onovenia
-const float g = -9.80665; //m/s^2
+const float g = 9.80665; //m/s^2
+const float ay = -9.80665;
 const float PI = 3.14159265358979323846;
 const int num_segments = 100; // pre kreslenie kruhu
-const float anglePerSegment = 2.0 * PI / num_segments;
+const float anglePerSegment = 2.0 * 3.14159265358979323846 / 100;
 
-float r, ysur, xsur = 0,
+float r, ysur0, ysur, xsur = 0,
         v0, v0y, v0x, v,
         ymax, xmax = 0,
         tymax, txmax,
         vmax,
-        t = -0.01;
+        t = -0.01,
+        alfa;
 
 
 
-int alfa; // uhol ktory zavisi od toho kam bol vyhodeny objekt
 int atHighest; // boolean premenna ktora povie ci objekt uz bol v najvyssom bode
 
 FILE* file_txt; // zapisujem data do dvoch suborov, txt na odovzdanie a dat na vytvorenie grafu kedze
@@ -47,8 +48,8 @@ void aktualizuj(const int ihod){
 
     printf("time in seconds: %.2fs, was at highest %d\n", t, atHighest);
 
-    vy = v0y - g*t;
-    ysur = vy * t;
+    vy = v0y + ay*t;
+    ysur = ysur0 + v0y * t + ay * t * t / 2;
     xsur = v0x * t;
     v = sqrt(vy*vy + v0x*v0x);
 
@@ -87,11 +88,28 @@ void Sikmy_Vrh(){
     glTranslatef(xsur/xmax, ysur/ymax, 0);
 
 
+//    glBegin(GL_QUADS);
+//
+//    glColor3f(1.0,0.0,0.0);
+//    glVertex2f(0.0, 0.0);
+//
+//    glColor3f(0.5,0.5,0.0);
+//    glVertex2f(0.0 , 0.0);
+//
+//    glColor3f(0.0,0.5,0.5);
+//    glVertex2f(0.2 , 0.0);
+//
+//    glColor3f(0.0,0.0,1.0);
+//    glVertex2f(0.2 , 0.0);
+//
+//
+//    glEnd();
+
     glBegin(GL_TRIANGLE_FAN);
     for (int i = 0; i < num_segments; i++) {
         float theta = i / anglePerSegment;
-        float x = r * cosf(theta);
-        float y = r * sinf(theta);
+        float x = r/100 * cosf(theta);
+        float y = r/100 * sinf(theta);
         glVertex2f(x, y);
     }
     glEnd();
@@ -105,12 +123,8 @@ void obsluhaResize(int sirka, int vyska){
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-
-//    if(sirka == 0) sirka++;
-//  const float pomstr = (GLfloat)vyska/(GLfloat)sirka;
-//    if (sirka >= vyska) gluOrtho2D(-1.0*pomstr*a, 1.0*pomstr*a, -1.0*a, 1.0*a);
-//    else gluOrtho2D(0, xmax, 0, ymax);
-    gluOrtho2D(0, xmax, 0, ymax);
+    
+    gluOrtho2D(0, xmax/100, 0, 1.0);
 
 }
 
@@ -118,20 +132,21 @@ void obsluhaResize(int sirka, int vyska){
 
 int main(int argc, char **argv){
 
-    scanf("%f %f %d %f", &ysur, &v, &alfa, &r);
+    scanf("%f %f %f %f", &ysur0, &v, &alfa, &r);
 
-    float radians = alfa * PI / 180.0;
-
-    v0x = v0 * cos(radians);
-    v0y = v0 * sin(radians);
+    float radians = alfa * M_PI / 180.0;
+    v0x = v * cos(radians);
+    v0y = v * sin(radians);
 
     file_txt = fopen("uloha4.txt","w");
     file_dat = fopen("uloha4.dat","w");
     if (v0y > 0) tymax = v0y / g;   //cas za ktory kruh dosihne najvyssej pozicie
     else tymax = 0.0;
 
+    printf("ysur = %.2fm, alfa = %.2f, v0y = %.2fm/s, v0x = %.2fm/s, tymax = %.2f s\n", ysur0, alfa, v0y, v0x, tymax);
 
-    ymax = tymax * g / 2 + ysur; // maximalne dosiahnutelna vyska
+
+    ymax = tymax * g / 2 + ysur0; // maximalne dosiahnutelna vyska
     txmax = 2 * v0y / g;
     xmax = v0x * txmax;
 
@@ -143,12 +158,12 @@ int main(int argc, char **argv){
             "\n", ymax, tymax );
 
     fprintf(file_txt, "#-Hlavicka-#\n"
-                      "# y0 = %.2fm, v0 = %.2fm/s, alfa = %ddegrees, r = %.2fm,\n"
-                      "# tD = %.2fs, xmax = %.2fm, tm = %.2fs, ymax = %.2f\n#----------#\n", ysur, v, alfa, r,
+                      "# y0 = %.2fm, v0 = %.2fm/s, alfa = %.0f degrees, r = %.2fm,\n"
+                      "# tD = %.2fs, xmax = %.2fm, tm = %.2fs, ymax = %.2f\n#----------#\n", ysur0, v, alfa, r,
                                                                                         txmax, xmax, tymax, ymax);
 
-    fprintf(file_txt, "%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n", t, xsur, ysur, v0x, v0y, v);
-    fprintf(file_dat, "%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n", t, xsur, ysur, v0x, v0y, v);
+    fprintf(file_txt, "%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n", t, xsur, ysur0, v0x, v0y, v);
+    fprintf(file_dat, "%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n", t, xsur, ysur0, v0x, v0y, v);
 
 
 
@@ -158,10 +173,10 @@ int main(int argc, char **argv){
 
     if (xmax >= ymax) {
         windowWidth = 1080;
-        windowHeight = 1080 * (ymax / xmax);
+        windowHeight = 1080 * round(ymax / xmax);
     }
     else  {
-        windowWidth = 1080 * (xmax / ymax);
+        windowWidth = 1080 * round(xmax / ymax);
         windowHeight = 1080;
     }
 
